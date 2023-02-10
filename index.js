6fc325b20114df69d4f07b4e7ec5a215494caa68
fahfhi <fah1204fah@gmail.com>
2023-02-10 11:03:09 +0700

    main committed

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const routes = require('./routes')
+var cors = require('cors')
+var path = require('path')
+const app = express()
+app.use(cors())
+
+app.use(function(req, res, next){
+  res.setTimeout(5000, function(){
+      console.log('Request has timed out.');
+          res.send(408);
+      });
+
+  next();
+});
+
+app.use((req, res, next) => {
+  
+    const auth = {login: 'admin', password: 'password'}
+  
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  
+    // Verify login and password are set and correct
+    if (login && password && login === auth.login && password === auth.password) {
+      // Access granted...
+      return next()
+    }
+  
+    // Access denied...
+    res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+    res.status(401).send('Authentication required.') // custom message
+  })
+
+app.use(routes)
+
+app.listen(8500, '0.0.0.0', () => {
+    console.log(`Server started at ${8500}`)
+});
---
+++ routes.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const { getRun, insertRun, updateRun, deleteRun, getOneRun, getDetailCardRun, insertDetailCardRun, getStyleCardRun, insertStyleCardRun, login } = require('./app');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
+
+//check API connected
+router.get('/check', async (req, res) => {
+    try{
+        res.send('API outed')
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+//getRun
+router.get('/get', async (req, res) => {
+      try{
+        const output = await getRun()
+        res.send(output)
+      }
+      catch(error){
+        res.status(500).json({message: error.message})
+      };
+  })
+
+//getOneRun
+router.get('/getOne', async(req, res) => {
+    try{
+        const output = await getOneRun(req.body)
+        res.send(output)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+router.get('/login', async(req,res) => {
+    try{
+        const output = await login(req.body)
+        res.send(output)
+    }
+    catch(error){
+        res.status(500).json({mesaage: error.message})
+    }
+})
+
+//getDetailCardRun
+router.get('/getOne/detailCard', async(req, res) => {
+    try{
+        const output = await getDetailCardRun(req.body)
+        res.send(output)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+router.get('/getOne/styleCard', async(req, res) => {
+    try{
+        const output = await getStyleCardRun(req.body)
+        res.send(output)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//insertRun
+router.post('/insert', async(req, res) => {
+    try{
+        const output = await insertRun(req.body)
+        res.send(output)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//insertDetailCardRun
+router.post('/insert/detailCard', async(req,res) => {
+    try{
+        const output = await insertDetailCardRun(req.body)
+        res.send(output)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//insertStyleCardRun
+router.post('/insert/styleCard', async(req, res) => {
+    try{
+        const output = await insertStyleCardRun(req.body)
+        res.send(output)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//updateRun
+router.put('/update', async (req, res) => {
+    try{
+        const output = await updateRun(req.body)
+        res.send(output)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+  })
+
+//deleteRun
+router.delete('/delete', async (req, res) => {
+    try{
+        const output = await deleteRun(req.body)
+        res.send(output)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+module.exports = router;
+  
